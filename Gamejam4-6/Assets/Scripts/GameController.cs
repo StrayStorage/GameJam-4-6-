002e3c5347e5cd49651a4df059cc7a5b69634ae1
samtanz <samueltanz91@gmail.com>
2022-06-04 20:21:28 +0800

    cleanup
    -cleanup
    -ui updates

--- Gamejam4-6/Assets/Scripts/GameController.cs
+++ Gamejam4-6/Assets/Scripts/GameController.cs
@@ -1,10 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class GameController : MonoBehaviour
 {
     public static GameController Instance;
+
+    [Header("Live Variables")]
+    public int currentResources;
+
+    [Header("Game Properties")]
+    public int totalResourceGiven = 24;
+
+    public List<int> costOfTower;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +25,8 @@ public class GameController : MonoBehaviour
         {
             Instance = this;
         }
+
+        currentResources = totalResourceGiven;
     }
 
     // Update is called once per frame
--- Gamejam4-6/Assets/Scripts/SelectTowerSpawn.cs
+++ Gamejam4-6/Assets/Scripts/SelectTowerSpawn.cs
@@ -2,29 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 public class SelectTowerSpawn : MonoBehaviour
 {
     public int currentTowerIndex;
 
-
-    public List<Toggle> toggleList;
-
-    public List<Sprite> spriteList;
-
-
     public Image activeTower;
 
+    public Text resourceCountRef;
 
+    public List<TowerData> towerDataList = new List<TowerData>();
     // Start is called before the first frame update
     void Start()
     {
-        toggleList[0].isOn = true;
-        activeTower.sprite = spriteList[0];
+        towerDataList[0].ToggleRef.isOn = true;
+        activeTower.sprite = towerDataList[0].SpriteRef;
     }
 
     // Update is called once per frame
     void Update()
     {
+        resourceCountRef.text = GameController.Instance.currentResources + "/" + GameController.Instance.totalResourceGiven;
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("MouseDown");
+            RaycastHit hit;
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider != null)
+                {
+
+                    if (GameController.Instance.currentResources > 0)
+                    {
+                        if ((GameController.Instance.currentResources - GameController.Instance.costOfTower[currentTowerIndex]) >= 0)
+                        {
+                            GameController.Instance.currentResources -= GameController.Instance.costOfTower[currentTowerIndex];
+                            GameObject newTower = Instantiate(towerDataList[currentTowerIndex].PrefabRef, hit.point, Quaternion.identity, this.transform) as GameObject;
+                        }
+                        else
+                        {
+                            Debug.Log("Insufficient Resource");
+                        }
+
+                       
+                    }
+
+                    
+                }
+            }
+        }
+
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             MakeFocus(0);
@@ -39,16 +70,16 @@ public class SelectTowerSpawn : MonoBehaviour
     public void MakeFocus(int num)
     {
         currentTowerIndex = num;
-        toggleList[num].isOn = true;
-        activeTower.sprite = spriteList[num];
+        towerDataList[num].ToggleRef.isOn = true;
+        activeTower.sprite = towerDataList[num].SpriteRef;
     }
     public void CheckFocus(Toggle togRef)
     {
         if (togRef.isOn)
         {
-            for (int i = 0; i < toggleList.Count; i++)
+            for (int i = 0; i < towerDataList.Count; i++)
             {
-                if (togRef == toggleList[i])
+                if (togRef == towerDataList[i].ToggleRef)
                 {
                     MakeFocus(i);
                 }
@@ -56,3 +87,23 @@ public class SelectTowerSpawn : MonoBehaviour
         }
     }
 }
+
+[Serializable]
+public class TowerData
+{
+    public Toggle ToggleRef;
+
+    public Sprite SpriteRef;
+
+    public GameObject PrefabRef;
+
+    public TowerData(Toggle toggleRef, Sprite spriteRef, GameObject prefabRef)
+    {
+        ToggleRef = toggleRef;
+
+        SpriteRef = spriteRef;
+
+        PrefabRef = prefabRef;
+    }
+}
+
