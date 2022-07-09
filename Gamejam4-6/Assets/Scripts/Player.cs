342dbef4d46c75cb867b2312487bf9f422025ab5
samtanz <samueltanz91@gmail.com>
2022-07-09 18:27:41 +0800

    Player healthbar
    -reorganized player code
    -added healthbar and health values

--- Gamejam4-6/Assets/Scripts/Player.cs
+++ Gamejam4-6/Assets/Scripts/Player.cs
@@ -1,32 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Player : MonoBehaviour
 {
     private CharacterController controller;
     private Vector3 playerVelocity;
+    private AttachToPlayer cameraFollowRef;
+    public Animator anima;
+    public LineRenderer lr;
 
     private float playerSpeed = 2.0f;
     private float gravityValue = -9.81f;
-    public Animator anima;
+
+    public float currentHealth;
+    public float maxHealth = 100;
+
+
     private Vector3 mousePos;
     private Vector3 mouseWorldPos;
-
     private float distFromScreen;
-    public LineRenderer lr;
-    public float rotationSpeed = 20.0f;
-
-    private AttachToPlayer cameraFollowRef;
+    private float rotationSpeed = 20.0f;
+    
+    
+    [Header("Player UI Properties")]
+    public Slider sliderVal;
 
     private void Start()
     {
         controller = GetComponent<CharacterController>();
         cameraFollowRef = Camera.main.transform.parent.GetComponent<AttachToPlayer>();
+        currentHealth = maxHealth;
     }
 
     void Update()
     {
+        HealthUpdate();
+
         if (cameraFollowRef.enableFollow)
         {
             distFromScreen = 2f;
@@ -62,11 +72,20 @@ public class Player : MonoBehaviour
 
         Vector3 targetDirection = mouseWorldPos - transform.position;
 
-        float singleStep = rotationSpeed;// * Time.deltaTime;
-
         // Rotate the forward vector towards the target direction by one step
-        Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, singleStep, 0.0f);
+        Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, rotationSpeed, 0.0f);
         transform.rotation = Quaternion.LookRotation(new Vector3(newDirection.x, 0, newDirection.z));
+    }
+
+    void HealthUpdate()
+    {
+        float newVal = currentHealth / maxHealth;
+
+        sliderVal.value = newVal;
+    }
+    public void TakeDamage(float takenDmg)
+    {
+        currentHealth -= takenDmg;
     }
 
     /*
